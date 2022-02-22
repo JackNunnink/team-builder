939b786b6b22e4b5febeae7818c189cfa624d4d2
Jack Nunnink <jack.nunnink@protonmail.com>
2022-02-22 15:58:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,23 @@ const initialValue = {
 }
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+  const [values, setValues] = useState(initialValue);
+  const [errors, setErrors] = useState(null);
+
+  const updateForm = (inputName, inputValue) => {
+    setValues({ ...values, [inputName]: inputValue });
+  }
+
+  const submitForm = () => {
+    const newCustomer = {
+      fullName: values.fullName.trim(),
+      phoneNumber: values.phoneNumber.trim(),
+      email: values.email.trim(),
+      websiteType: values.websiteType
+    }
+  }
+
   return (
     <div className="App">
 
