632d897d208b72ee489432342e3d81ca6a6d9b57
Jack Nunnink <jack.nunnink@protonmail.com>
2022-02-22 15:44:03 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import Customer from './components/Customer';
+import CustomerForm from './components/CustomerForm';
+import './App.css';
+
+const initialValue = {
+  fullName: '',
+  phoneNumber: '',
+  email: '',
+  websiteType: ''
+}
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme.js/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#003049',
+    secondaryColor: '#c1121f',
+    textColor: '#fdf0d5',
+    highlightColor: '#669bbc',
+    highlightColor2: '#c1121f',
+    breakpointDesktop: '(min-width: 550px)'
+}
